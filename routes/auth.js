83ace06adcfa92419adc48157799bb30d2df354e
VicenteIV <josevicentet46@gmail.com>
2025-02-08 20:47:39 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -83,7 +83,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://p2-27963818.onrender.com/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       const email = profile.emails[0].value;
